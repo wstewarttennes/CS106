726a2e772f35eb7e017f5898e4258d39bbe8b09a
Stewart-Tennes <wstewart@cslab19.cs.skidmore.edu>
2014-09-03 20:06:16 -0400

    Initial commit.

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author wstewart
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\"I like nice people!\" ");
+	}
+
+}
