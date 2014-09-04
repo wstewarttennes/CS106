4519f8a4d389b4d48cb131b6c2438105767df18d
Weston <westonstewarttennes@westons-mbp.skidmore.edu>
2014-09-03 20:33:36 -0400

    initial commit.

---
+++ This is/src/ThisIs.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author westonstewarttennes
+ *
+ */
+public class ThisIs {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
