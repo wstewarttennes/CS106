0d92bf606adb7bcd0d386f6f98c758e7893b9375
Weston <westonstewarttennes@westons-mbp.skidmore.edu>
2014-09-29 10:33:14 -0400

    first commit whatX

---
+++ Smiley/src/Smile.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author westonstewarttennes
+ *
+ */
+public class Smile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
