2b5fcea4683cd4cd7d18981c52995ba48553678e
Weston <westonstewarttennes@westons-mbp.skidmore.edu>
2014-10-22 18:55:18 -0400

    yo tho

---
+++ practice/src/Practice.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+/**
+ * @author westonstewarttennes
+ *
+ */
+public class Practice {
+	
+	public static void main(String[] args) {
+		Point p = new Point(3,2);
+		p.translate(4,5);
+		System.out.print(p);
+
+	}
+
+}
